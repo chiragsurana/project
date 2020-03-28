4183d24db5e7937705a2040e4a6b2ef170cd6857
chiragsurana <57294330+chiragsurana@users.noreply.github.com>
2020-03-28 12:12:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+  document.querySelector(".menu").addEventListener('click',
+      function() {
+          document.querySelector('.photo').style.display = 'flex';
+      }
+  )
+  document.querySelector(".button", ".menu").addEventListener('click',
+      function() {
+          document.querySelector('.photo').style.display = 'none';
+      }
+  )
+
+
+
+  document.querySelector(".menu1").addEventListener('click',
+      function() {
+          document.querySelector('.photo1').style.display = 'flex';
+      }
+  )
+  document.querySelector(".button1").addEventListener('click',
+      function() {
+          document.querySelector('.photo1', ).style.display = 'none';
+      }
+  )
+
+
+  document.querySelector(".menu2").addEventListener('click',
+      function() {
+          document.querySelector('.photo2').style.display = 'flex';
+      }
+  )
+  document.querySelector(".button2").addEventListener('click',
+      function() {
+          document.querySelector('.photo2', ).style.display = 'none';
+      }
+  )
+
+  document.querySelector(".menu3").addEventListener('click',
+      function() {
+          document.querySelector('.photo3').style.display = 'flex';
+      }
+  )
+  document.querySelector(".button3").addEventListener('click',
+      function() {
+          document.querySelector('.photo3', ).style.display = 'none';
+      }
+  )
